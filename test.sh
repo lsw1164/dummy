fd57fbd4bae0244bd67ebf78fe222ce084cb8982
lsw1164 <qkqkfo@gmail.com>
2019-12-13 23:38:41 +0900

    test...

---
+++ test.sh
@@ -0,0 +1,8 @@
+
+~/myassets/auto_commit.sh -d  26 -n 1
+~/myassets/auto_commit.sh -d  25 -n 1
+~/myassets/auto_commit.sh -d  24 -n 4
+~/myassets/auto_commit.sh -d  23 -n 4
+~/myassets/auto_commit.sh -d  22 -n 8
+~/myassets/auto_commit.sh -d  21 -n 8
+~/myassets/auto_commit.sh -d  20 -n 11
